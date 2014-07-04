8db556b9a297a05addca23865223901002e469b1
tongwustap <tong.wu.stap@gmail.com>
2014-07-04 23:08:39 +0800

    add retry type

--- RetryLib/Retry.cs
+++ RetryLib/Retry.cs
@@ -9,11 +9,15 @@ namespace RetryLib
 {
     public enum RetryType{
         Linear,
-        Double
+        Double,
+        Zero
     }
 
     public class Retry
     {
+        const int DefaultRetryCount = 3;
+        const int DefaultInternalSecond = 5;
+
         public Predicate<Exception> RetryPolicy { get; set; }
 
         public int RetryCount { get; set; }
@@ -28,6 +32,12 @@ namespace RetryLib
             InternalTime = internalTime;
             RetryType = retryType;
         }
+
+        public Retry(int retryCount = DefaultRetryCount, 
+                     int internalSecond = DefaultInternalSecond, 
+                     RetryType retryType = RetryType.Linear)
+            : this(retryCount, TimeSpan.FromSeconds(internalSecond), retryType)
+        {  }
 
         public void ExecuteAction(Action action)
         {
@@ -69,11 +79,15 @@ namespace RetryLib
         {
             switch (RetryType)
             {
-                case RetryType.Linear:
+                case RetryType.Double:
+                    return TimeSpan.FromSeconds(InternalTime.Seconds * Math.Pow(2, currentRetryTime));
+                case RetryType.Zero:
+                    return TimeSpan.Zero;
+
+                default:
                     return InternalTime;
             }
 
-            return TimeSpan.Zero;
         }
     }
 }
